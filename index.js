ca33dbd46920b0b8cf4721a9ef453a18bdaf2404
El profe <cheo.alfredo@gmail.com>
2021-07-18 10:34:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res, next) => {
-	res.json({done: "yes", ready : true});
+	res.json({done: "yes", ready : true, devtron: true});
 });
 
 app.listen(3000, () => {
